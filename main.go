4c23a50f8a6afef5ddf7710d1fc8ca3d47a5d940
ArgentiLover <mofischl00@gmail.com>
2024-11-06 13:27:26 +0300

    add summm

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
-package main 
+package main
 
-import {
+import (
 	"fmt"
-}
+)
 
 func main() {
-	fmt.Println(summ(2,5))
+	fmt.Println(summm(2, 5, 8))
 }
 
-func summ(num1,num2 int) int {
-	return num1+num2
+func summ(num1, num2 int) int {
+	return num1 + num2
+}
+
+func summm(num1, num2, num3 int) int {
+	return num1 + num2 + num3
 }
